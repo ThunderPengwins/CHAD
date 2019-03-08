ed16e636a966230d8aa9f53bb53a1cc85f6ad3bf
Eric <3rix40@gmail.com>
2019-03-07 20:09:30 -0600

    Start main Intro Page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <div class="bkgd">
+      <h1 id="title">Is this working?</h1>
+      <p class ="words">
+        Welcome to CHAD!<br/>This is where you do all of your autonomous coding forever more.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -21,5 +27,24 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#title{
+  color: green;
+}
+
+.bkgd{
+  top: 5%;
+  bottom: 15%;
+  left: 3%;
+  right: 3%;
+  position: absolute;
+  border-radius: 5px;
+  background-color: lightgrey;
+}
+
+.words{
+  text-align: left;
+  margin-left: 3%;
 }
 </style>
